69a426c55d3e886a142826e498c0303803c699b9
Timothy McPhillips <tmcphillips@absoluteflow.org>
2017-06-05 12:19:55 -0700

    New test project for testing yw-parse.

---
+++ visual-studio/yw-parse-tests/listener_tests.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace yw_parse_tests
+{		
+	TEST_CLASS(ListenerTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
