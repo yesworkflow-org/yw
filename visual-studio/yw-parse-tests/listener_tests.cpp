a9ec1f6c3350bd9c77b2b5866a14b7f472755f70
Timothy McPhillips <tmcphillips@absoluteflow.org>
2017-06-05 17:09:16 -0700

    Added first test exercising YW parser.

--- visual-studio/yw-parse-tests/listener_tests.cpp
+++ visual-studio/yw-parse-tests/listener_tests.cpp
@@ -1,18 +1,46 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 
+#include "YWLexer.h"
+#include "YWListener.h"
+#include "YWParser.h"
+#include "YWBaseListener.h"
+
+#include <sstream>
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using std::stringstream;
 
 namespace yw_parse_tests
-{		
+{
+	class YWListenerForTests : public YWBaseListener {
+	private:
+		stringstream _log;
+	public:
+		inline std::string log() { return _log.str(); }
+		void enterBegin(YWParser::BeginContext *context) override {
+			_log << "entered begin";
+		}
+	};
+
 	TEST_CLASS(ListenerTests)
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TestEnterBegin)
 		{
-			// TODO: Your test code here
-		}
+			stringstream ss("@begin foo");
 
+			antlr4::ANTLRInputStream input(ss);
+			YWLexer lexer(&input);
+			antlr4::CommonTokenStream tokens(&lexer);
+			YWParser parser(&tokens);
+
+			antlr4::tree::ParseTree *tree = parser.begin();
+			YWListenerForTests listener;
+			antlr4::tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
+
+			Assert::AreEqual(std::string("entered begin"), listener.log());
+		}
 	};
 }
--- visual-studio/yw-parse-tests/stdafx.h
+++ visual-studio/yw-parse-tests/stdafx.h
@@ -11,3 +11,6 @@
 #include "CppUnitTest.h"
 
 // TODO: reference additional headers your program requires here
+#include <iostream>
+
+#include "antlr4-runtime.h"
