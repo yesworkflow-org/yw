a58cc30f9d023a78b45602be7b42627a6b749842
Timothy McPhillips <tmcphillips@absoluteflow.org>
2017-06-06 17:12:13 -0700

    Added new test and updated first test to reflect changes in test listener class

--- visual-studio/yw-parse-tests/listener_tests.cpp
+++ visual-studio/yw-parse-tests/listener_tests.cpp
@@ -10,6 +10,7 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using std::stringstream;
+using std::endl;
 
 namespace yw_parse_tests
 {
@@ -17,13 +18,17 @@ namespace yw_parse_tests
 	private:
 		stringstream _log;
 	public:
+
 		inline std::string log() { return _log.str(); }
-		void enterBegin(YWParser::BeginContext *context) override {
-			_log << "entered begin";
-		}
-		void reset() {
-			_log.clear();
-		}
+
+		void enterScript(YWParser::ScriptContext *context) override { _log << "entered script" << endl;  }
+		void exitScript(YWParser::ScriptContext *context) override { _log << "exited script" << endl; }
+		void enterCodeBlock(YWParser::CodeBlockContext *context) override { _log << "entered block" << endl; }
+		void exitCodeBlock(YWParser::CodeBlockContext *context) override { _log << "exited block" << endl; }
+		void enterBegin(YWParser::BeginContext *context) override { _log << "entered begin" << endl; }
+		void exitBegin(YWParser::BeginContext *context) override { _log << "exited begin" << endl; }
+		void enterEnd(YWParser::EndContext *context) override { _log << "entered end" << endl; }
+		void exitEnd(YWParser::EndContext *context) override { _log << "exited end" << endl; }
 	};
 
 	TEST_CLASS(ListenerTests)
@@ -38,21 +43,40 @@ namespace yw_parse_tests
 			YWLexer lexer(&input);
 			antlr4::CommonTokenStream tokens(&lexer);
 			YWParser parser(&tokens);
-			antlr4::tree::ParseTree *tree = parser.begin();
+			antlr4::tree::ParseTree *tree = parser.script();
 			antlr4::tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
 		}
 
 	public:
-		
-		TEST_METHOD_INITIALIZE(InitializeListenerTest) {
-			listener.reset();
-		}
 
 		TEST_METHOD(TestEnterBegin)
 		{
 			parse("@begin foo");
-			Assert::AreEqual(std::string("entered begin"), listener.log());
+			
+			Assert::AreEqual(std::string(
+				"entered script"	"\n"
+				"entered block"		"\n"
+				"entered begin"		"\n"
+				"exited begin"		"\n"
+				"exited block"		"\n"
+				"exited script"		"\n"
+			), listener.log());
 		}
 
+		TEST_METHOD(TestEnterEnd)
+		{
+			parse("@begin bar @end bar");
+
+			Assert::AreEqual(std::string(
+				"entered script"	"\n"
+				"entered block"		"\n"
+				"entered begin"		"\n"
+				"exited begin"		"\n"
+				"entered end"		"\n"
+				"exited end"		"\n"
+				"exited block"		"\n"
+				"exited script"		"\n"
+			), listener.log());
+		}
 	};
 }
