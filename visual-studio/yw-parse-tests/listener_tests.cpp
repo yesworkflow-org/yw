64adaf64c37ff818b5f729cc399bfebe28d70c43
Timothy McPhillips <tmcphillips@absoluteflow.org>
2017-06-07 20:02:03 -0700

    Enabled custom grammar entry point in each listener event sequence test

--- visual-studio/yw-parse-tests/listener_tests.cpp
+++ visual-studio/yw-parse-tests/listener_tests.cpp
@@ -18,9 +18,7 @@ namespace yw_parse_tests
 	private:
 		stringstream _log;
 	public:
-
-		inline std::string log() { return _log.str(); }
-
+		std::string log() { return _log.str(); }
 		void enterScript(YWParser::ScriptContext *context) override { _log << "entered script" << endl;  }
 		void exitScript(YWParser::ScriptContext *context) override { _log << "exited script" << endl; }
 		void enterBlock(YWParser::BlockContext *context) override { _log << "entered block" << endl; }
@@ -35,8 +33,8 @@ namespace yw_parse_tests
 		void exitEnd(YWParser::EndContext *context) override { _log << "exited end" << endl; }
 		void enterEndKeyword(YWParser::EndKeywordContext *context) override { _log << "entered end keyword" << endl; }
 		void exitEndKeyword(YWParser::EndKeywordContext *context) override { _log << "exited end keyword" << endl; }
-		void enterBlockAttribute(YWParser::BlockAttributeContext *context) override { _log << "entered block qualifier" << endl; }
-		void exitBlockAttribute(YWParser::BlockAttributeContext *context) override { _log << "exited block qualifier" << endl; }
+		void enterBlockAttribute(YWParser::BlockAttributeContext *context) override { _log << "entered block attribute" << endl; }
+		void exitBlockAttribute(YWParser::BlockAttributeContext *context) override { _log << "exited block attribute" << endl; }
 		void enterPort(YWParser::PortContext *context) override { _log << "entered port" << endl; }
 		void exitPort(YWParser::PortContext *context) override { _log << "exited port" << endl; }
 		void enterInputPort(YWParser::InputPortContext *context) override { _log << "entered input port" << endl; }
@@ -45,8 +43,12 @@ namespace yw_parse_tests
 		void exitOutputPort(YWParser::OutputPortContext *context) override { _log << "exited output port" << endl; }
 		void enterIn(YWParser::InContext *context) override { _log << "entered in" << endl; }
 		void exitIn(YWParser::InContext *context) override { _log << "exited in" << endl; }
+		void enterParam(YWParser::ParamContext *context) override { _log << "entered param" << endl; }
+		void exitParam(YWParser::ParamContext *context) override { _log << "exited param" << endl; }
 		void enterOut(YWParser::OutContext *context) override { _log << "entered out" << endl; }
 		void exitOut(YWParser::OutContext *context) override { _log << "exited out" << endl; }
+		void enterRet(YWParser::RetContext *context) override { _log << "entered ret" << endl; }
+		void exitRet(YWParser::RetContext *context) override { _log << "exited ret" << endl; }
 	};
 
 	TEST_CLASS(ListenerTests)
@@ -54,22 +56,25 @@ namespace yw_parse_tests
 	private:
 		
 		YWListenerForTests listener;
+		stringstream* textStream;
+		antlr4::ANTLRInputStream* input;
+		YWLexer* lexer;
+		antlr4::CommonTokenStream* tokens;
 
-		void parse(const char * text) {
-			stringstream textStream(text);
-			antlr4::ANTLRInputStream input(textStream);
-			YWLexer lexer(&input);
-			antlr4::CommonTokenStream tokens(&lexer);
-			YWParser parser(&tokens);
-			antlr4::tree::ParseTree *tree = parser.script();
-			antlr4::tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
+		YWParser* parse(const char * text) {
+			textStream = new stringstream(text);
+			input = new antlr4::ANTLRInputStream(*textStream);
+			lexer = new YWLexer(input);
+			tokens = new antlr4::CommonTokenStream(lexer);
+			return new YWParser(tokens);
 		}
 
 	public:
 
 		TEST_METHOD(TestListenerEventSequence_Begin_End)
 		{
-			parse("@begin b @end b");
+			YWParser* parser = parse("@begin b @end b");
+			antlr4::tree::ParseTreeWalker::DEFAULT.walk(&listener, parser->script());
 
 			Assert::AreEqual(std::string(
 				"entered script"		"\n"
@@ -93,10 +98,11 @@ namespace yw_parse_tests
 
 		TEST_METHOD(TestListenerEventSequence_Begin_In_End)
 		{
-			parse("@begin b @in p @end b");
+			
+			YWParser* parser = parse("@begin b @in p @end b");
+			antlr4::tree::ParseTreeWalker::DEFAULT.walk(&listener, parser->block());
 
 			Assert::AreEqual(std::string(
-				"entered script"			"\n"
 				"entered block"				"\n"
 				"entered begin"				"\n"
 				"entered begin keyword"		"\n"
@@ -104,14 +110,14 @@ namespace yw_parse_tests
 				"entered block name"		"\n"
 				"exited block name"			"\n"
 				"exited begin"				"\n"
-				"entered block qualifier"	"\n"
+				"entered block attribute"	"\n"
 				"entered port"				"\n"
 				"entered input port"		"\n"
 				"entered in"				"\n"
 				"exited in"					"\n"
 				"exited input port"			"\n"
 				"exited port"				"\n"
-				"exited block qualifier"	"\n"
+				"exited block attribute"	"\n"
 				"entered end"				"\n"
 				"entered end keyword"		"\n"
 				"exited end keyword"		"\n"
@@ -119,16 +125,15 @@ namespace yw_parse_tests
 				"exited block name"			"\n"
 				"exited end"				"\n"
 				"exited block"				"\n"
-				"exited script"				"\n"
 			), listener.log());
 		}
 
 		TEST_METHOD(TestListenerEventSequence_Begin_Out_End)
 		{
-			parse("@begin b @out p @end b");
+			YWParser* parser = parse("@begin b @out p @end b");
+			antlr4::tree::ParseTreeWalker::DEFAULT.walk(&listener, parser->block());
 
 			Assert::AreEqual(std::string(
-				"entered script"			"\n"
 				"entered block"				"\n"
 				"entered begin"				"\n"
 				"entered begin keyword"		"\n"
@@ -136,14 +141,14 @@ namespace yw_parse_tests
 				"entered block name"		"\n"
 				"exited block name"			"\n"
 				"exited begin"				"\n"
-				"entered block qualifier"	"\n"
+				"entered block attribute"	"\n"
 				"entered port"				"\n"
 				"entered output port"		"\n"
 				"entered out"				"\n"
 				"exited out"				"\n"
 				"exited output port"		"\n"
 				"exited port"				"\n"
-				"exited block qualifier"	"\n"
+				"exited block attribute"	"\n"
 				"entered end"				"\n"
 				"entered end keyword"		"\n"
 				"exited end keyword"		"\n"
@@ -151,10 +156,75 @@ namespace yw_parse_tests
 				"exited block name"			"\n"
 				"exited end"				"\n"
 				"exited block"				"\n"
-				"exited script"				"\n"
 			), listener.log());
 		}
 
+		TEST_METHOD(TestListenerEventSequence_In)
+		{
+			YWParser* parser = parse("@in p");
+			antlr4::tree::ParseTreeWalker::DEFAULT.walk(&listener, parser->blockAttribute());
 
+			Assert::AreEqual(std::string(
+				"entered block attribute"	"\n"
+				"entered port"				"\n"
+				"entered input port"		"\n"
+				"entered in"				"\n"
+				"exited in"					"\n"
+				"exited input port"			"\n"
+				"exited port"				"\n"
+				"exited block attribute"	"\n"
+			), listener.log());
+		}
+
+		TEST_METHOD(TestListenerEventSequence_Param)
+		{
+			YWParser* parser = parse("@param p");
+			antlr4::tree::ParseTreeWalker::DEFAULT.walk(&listener, parser->blockAttribute());
+
+			Assert::AreEqual(std::string(
+				"entered block attribute"	"\n"
+				"entered port"				"\n"
+				"entered input port"		"\n"
+				"entered param"				"\n"
+				"exited param"				"\n"
+				"exited input port"			"\n"
+				"exited port"				"\n"
+				"exited block attribute"	"\n"
+			), listener.log());
+		}
+
+		TEST_METHOD(TestListenerEventSequence_Out)
+		{
+			YWParser* parser = parse("@out p");
+			antlr4::tree::ParseTreeWalker::DEFAULT.walk(&listener, parser->blockAttribute());
+
+			Assert::AreEqual(std::string(				
+				"entered block attribute"	"\n"
+				"entered port"				"\n"
+				"entered output port"		"\n"
+				"entered out"				"\n"
+				"exited out"				"\n"
+				"exited output port"		"\n"
+				"exited port"				"\n"
+				"exited block attribute"	"\n"
+			), listener.log());
+		}
+
+		TEST_METHOD(TestListenerEventSequence_Return)
+		{
+			YWParser* parser = parse("@return p");
+			antlr4::tree::ParseTreeWalker::DEFAULT.walk(&listener, parser->blockAttribute());
+
+			Assert::AreEqual(std::string(
+				"entered block attribute"	"\n"
+				"entered port"				"\n"
+				"entered output port"		"\n"
+				"entered ret"				"\n"
+				"exited ret"				"\n"
+				"exited output port"		"\n"
+				"exited port"				"\n"
+				"exited block attribute"	"\n"
+			), listener.log());
+		}
 	};
 }
