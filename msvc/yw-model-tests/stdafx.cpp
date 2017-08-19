a3fcb2d5f549a91992e9a24647e7e0b20b87e360
Timothy McPhillips <tmcphillips@absoluteflow.org>
2017-08-18 21:03:44 -0700

    Added yw-model and yw-model-tests projects to YW solution.

---
+++ msvc/yw-model-tests/stdafx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : source file that includes just the standard includes
+// yw-model-tests.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "yw_model_tests.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
---
+++ msvc/yw-model-tests/yw_model_tests.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifdef _MSC_VER 
+#include "targetver.h"
+#endif
+
+#include <iostream>
+#include <sstream>
+
+#include "antlr4-runtime.h"
+#include "ywdb.h"
+#include "annotation_listener.h"
+#include "yw_parser_builder.h"
+#include "source_loader.h"
+#include "model_entity_listener.h"
+#include "yw_db_test_helpers.h"
