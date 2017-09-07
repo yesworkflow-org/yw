04557b61e97f275c107faf88f05d040c2845fd02
Timothy McPhillips <tmcphillips@absoluteflow.org>
2017-09-07 15:21:51 -0700

    The yw extract app now uses configuration options on command line.

--- src/yw-extract-app/yw_extract.cpp
+++ src/yw-extract-app/yw_extract.cpp
@@ -1,26 +1,41 @@
 #include "ywdb.h"
 #include "outline_exporter.h"
 #include "annotation_extractor.h"
+#include "yw_config.h"
+#include "command_line.h"
 
 #include <iostream>
 
 using namespace yw::extract;
+using namespace yw::config;
 
 int main(int argc, char** argv)
 {
     yw::db::YesWorkflowDB ywdb;
 
-    if (argc != 2) {
-        std::cout << "Usage:  yw-extract <path-to-script>" << std::endl;
-        exit(0);
-    }
+    //if (argc != 2) {
+    //    std::cout << "Usage:  yw-extract outline <path-to-script>" << std::endl;
+    //    exit(0);
+    //}
+
+    yw::cli::CommandLine commandLine{argc, argv};
+
+    Configuration configuration;
+    configuration.insertAll(commandLine.getSettings());
 
     try {
         AnnotationExtractor{ ywdb }.extractAnnotationsFromFile(argv[1]);
-        std::cout << OutlineExporter{ ywdb }.getOutline();
     }
     catch (std::runtime_error e) {
-        std::cerr << "ERROR: " << e.what() << std::endl;
+        std::cerr << "Error extracting annotations: " << e.what() << std::endl;
+        exit(-1);
+    }
+
+    try {
+        std::cout << OutlineExporter{ ywdb, configuration }.getOutline();
+    }
+    catch (std::runtime_error e) {
+        std::cerr << "Error exporting outline: " << e.what() << std::endl;
         exit(-1);
     }
 }
