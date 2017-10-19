77c4016cc87f4c62a496c1af490478ae8686c37c
Timothy McPhillips <tmcphillips@absoluteflow.org>
2017-10-19 13:54:04 -0700

    YW application now catches and reports otherwise uncaught exceptions at the top-most level.

--- src/yw-graph-app/yw_graph_main.cpp
+++ src/yw-graph-app/yw_graph_main.cpp
@@ -2,5 +2,14 @@
 
 int main(int argc, char** argv)
 {
-    return yw::graph::cli(yw::cli::CommandLine(argc, argv));
+    try {
+        return yw::graph::cli(yw::cli::CommandLine(argc, argv));
+    }
+    catch (std::exception e) {
+        std::cerr << "ERROR: An unexpected exception occurred." << std::endl;
+        std::cerr << "CAUSE: " << e.what() << std::endl;
+    }
+    catch (...) {
+        std::cerr << "ERROR: An unexpected and unrecogized exception occurred." << std::endl;
+    }
 }
