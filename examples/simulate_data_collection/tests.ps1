febe9bd68495cb6b5f5e6da5016d9e4e47fc0b5d
Timothy McPhillips <tmcphillips@absoluteflow.org>
2017-11-14 15:57:16 -0800

    Now validating arguments to test scripts and functions.

--- examples/simulate_data_collection/tests.ps1
+++ examples/simulate_data_collection/tests.ps1
@@ -1,11 +1,32 @@
 #!/usr/bin/env pwsh
 
+[cmdletbinding()]
+Param(
+	[ValidateSet('Windows','MacOS','Linux')] [string] $os       = $null,
+	[ValidateSet('x86','x64')]               [string] $platform = 'x64',
+	[ValidateSet('Debug','Release')]         [string] $build    = 'Release',
+	                                         [string] $exe      = $null
+)
+
 # work in the directory containing this script
 $MyInvocation.MyCommand.Path | Split-Path -Parent | Set-Location
 
-$RepoRoot = '../../'
-. $RepoRoot/pwsh/utilities.ps1
+# load utility functions
+. ../../pwsh/utilities.ps1
 
-$yw = Get-YesWorkflowPath Linux 
+$repoRoot = Get-YWRepositoryRoot
+
+if (!$os) { $os = Get-RuntimePlatform }
+"YW platform  : ${os}"
+
+if ($exe) {
+	$yw = $exe
+} else {
+	$yw = Get-YesWorkflowExe -repoRoot $repoRoot -os $os `
+							-platform $platform -build $build
+}
+
+"YW executable: $yw"
+
 & $yw graph simulate_data_collection.py graph.format=DOT graph.file=wf.dot
 Find-FirstDiff -actual 'wf.dot' -expected 'expected/wf.dot'
--- pwsh/utilities.ps1
+++ pwsh/utilities.ps1
@@ -1,31 +1,36 @@
 
+function Get-YWRepositoryRoot {
+    $MyInvocation.MyCommand.ScriptBlock.File | Split-Path -Parent | Split-Path -Parent
+}
 
-function Get-YesWorkflowPlatform {
+function Get-RuntimePlatform {
     if ($IsWindows) { 'Windows' } 
     elseif ($IsMacOS) { 'MacOS' }
     elseif ($IsLinux) { 'Linux' }
     else { 'Unknown' }
 }
 
-function Get-YesWorkflowPath {
+function Get-YesWorkflowExe {
 
 	[cmdletbinding()]
 	Param(
-        [string]$os,
-		[string]$platform='x64',
-        [string]$build='Release',
-        [string]$repoRoot='../..'
+        [ValidateSet('Windows','MacOS','Linux')] [string] $os,
+        [ValidateSet('x86','x64')]               [string] $platform = 'x64',
+        [ValidateSet('Debug','Release')]         [string] $build    = 'Release',
+                                                 [string]$repoRoot='../..'
 	)
 
     if ($os -eq "Windows") {
         if ($platform -eq 'x64') {
-            "${repoRoot}/msvc/x64/${build}/yw-graph-app.exe"
+            $exe = "${repoRoot}/msvc/x64/${build}/yw-graph-app.exe"
         } else {
-            "${repoRoot}/msvc/${build}/yw-graph-app.exe"
+            $exe = "${repoRoot}/msvc/${build}/yw-graph-app.exe"
         }
     } else {
-        "${repoRoot}/gcc/yw-graph-app/yw-graph-app"
+        $exe = "${repoRoot}/gcc/yw-graph-app/yw-graph-app"
     }
+
+    return Get-Item $exe | Select-Object -ExpandProperty FullName
 }
 
 function Out-FileDiff {
